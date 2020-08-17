ffd2caf450f0b6b4708053365724e7f8e02439ff
DESKTOP-0BVKIB9\Admin <marbanana.one@gmail.com>
2020-08-17 16:04:13 +0800

    update

--- application/controllers/DataList.php
+++ application/controllers/DataList.php
@@ -25,11 +25,23 @@ class DataList extends CI_Controller {
 	public function index(){
 		$page = $this->input->get('page') ? $this->input->get('page'):1;
 		$type =  $this->input->get('type') ?$this->input->get('type'):"";
-		$result =  $this->API->MATCH_LIST(['page'=>$page,'type'=>$type]);
+		$result =  $this->API->MATCH_LIST(['page'=>$page,'type'=>$type]);	
+		$detail =  $this->API->MATCH_BAR();				
 		if($result['status']){
 			$data['result'] = $result['data'];
+			$data['count'] = $result['count'];
+			$data['limit'] = $result['limit'];
+			$data['page'] = $result['page'];
+			$data['type']=$type;
 		}else{
 			$data['result'] = [];
+		}
+		
+		if($detail['status']){
+			$data['detail']=$detail['data'];
+			
+		}else{
+			$data['detail']=[];
 		}
 		$this->load->view('dataview',$data);
 	}
--- application/views/dataview.php
+++ application/views/dataview.php
@@ -8,24 +8,118 @@ $this->load->helper('url');
 <head>
 	<title>数据列表</title>
 	<meta name="referrer" content="no-referrer" />
-	<link rel="stylesheet" href="<?php echo base_url('public/pagination.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('public/simp.css');?>">
 	<style>
 		body{
 			padding: 0;
 			margin:0;
 			background:#f1f1f1;
 		}
+		a,a:hover{
+			text-decoration: none;
+		}
 		.main{
 			display: flex;
 			flex-wrap: wrap;
 			max-width: 1200px;
 			width: 100%;
-			margin:0 auto;
+			margin:0 auto;		
+			/* margin-top:20px; */
 		}
-		.box{
+		.main .nomore{
+			width: 100%;
+			text-align: center;
+			margin:25% 0;
+		}
+		.selecte{
+			max-width: 1200px;
+			width: 100%;
+			margin:0 auto;
+			position:relative;
+			display: flex;
+			background:#fff;
+			margin-bottom:20px;	
+		}
+		.box1{	
+			height:235px;
+			max-width: 1150px;
+			text-align: center;
+			display: flex;
+			flex-wrap:wrap;				
+			padding: 10px 0 ;
+			overflow: hidden;
+			transition: 500ms;
+		}
+		.box1.hide{
+			height:75px;			
+		}
+		.pagination{
+			padding:0;
+		}
+		.box1 .cur,.box1 .active,.box1 .cur:hover{
+			width:8%;
+			min-width:105px;
+			padding:5px 10px;
+			font-size:14px;		
+		}
+		.box1 .cur a,.box1 .active a,.box1 .cur:hover a{
+			display:inline-block;
+			width:95px;
+			padding:5px;
+			color:black;
+		}
+		.box1 .active a,.box1 div:hover a{			
+			box-shadow: rgba(0,0,0,.2) 0 2px 3px;			
+			border-radius:2px;
+		}
+		.selecte .main-right{
+			position: absolute;
+			top: 50px;
+			right: 16px;
+			width: 28px;
+			line-height: 28px;
+			height: 28px;
+			border: 1px solid #ddd;
+			border-radius: 2px;
+			text-align: center;
+			cursor: pointer;
+			background-color: #ededed;
+		}
+		.selecte .detele{
+			position: absolute;
+			top: 16px;
+			right: 16px;
+			width: 28px;
+			line-height: 28px;
+			font-size:14px;
+			height: 28px;
+			border: 1px solid #ddd;
+			border-radius: 2px;
+			text-align: center;
+			cursor: pointer;
+			background-color: #ededed;
+		}
+		.main-right span{
+			vertical-align: middle;
+			transition: 200ms;
+		}
+		
+		.main-right span::before{
+			content: '';
 			display: inline-block;
-			height: 500px;
-			width: calc((100% - 50px)/5);
+			width: 12px;
+			height: 12px;
+			background: url(/static/img/ic_arrow.svg) 0 0 no-repeat;
+			transition: transform .2s ease-in-out;
+		}
+		.main-right.rote span::before{
+			transform: rotate(180deg);
+		}
+		.whiteBox{
+			display: inline-block;
+			height: 120px;
+			max-width: 1200px;
+			width: 100%;
 			text-align: center;
 			background-color: #fff;
 			box-shadow: 0 1px 3px rgba(0,0,0,.02), 0 4px 8px rgba(0,0,0,.02);
@@ -33,6 +127,18 @@ $this->load->helper('url');
 			overflow: hidden;
 			margin: 5px;
 			font-size: 12px;
+			margin: 0 auto;
+			padding:10px 15px;
+			display: flex;
+			flex-wrap: wrap;
+		}
+		.whiteBox label{
+			width:10%;
+			min-width:100px;
+			text-align: left;			
+		}
+		.whiteBox label:hover{
+			cursor: pointer;
 		}
 		.match-bar{
 			height: 50px;
@@ -73,40 +179,252 @@ $this->load->helper('url');
 			    color: #909399;
 			    border: 1px solid #ebeef5;
 			    border-radius: 4px;
+		}		
+		input[type=number] {
+		    -moz-appearance:textfield;
 		}
+		input[type=number]::-webkit-inner-spin-button,
+		input[type=number]::-webkit-outer-spin-button {
+		    -webkit-appearance: none;
+		    margin: 0;
+		}
+		.jump{
+			margin:0 10px;
+			display: inline-block;
+			font-size: 15px;
+			line-height: 1;
+			-ms-user-select: none;
+			-moz-user-select: none;
+			-webkit-user-select: none;
+			user-select: none;
+		}
+		.jump input{
+			width:23px;
+			height:20px;
+			padding:0 5px;			
+			font-size: 14px;
+		}
+		.jump span:first-child{
+			margin-left:10px;
+			padding:5px;
+		}
+		.jump span:first-child:hover{
+			cursor: pointer;
+			background-color: #EAEAEA;
+		}
+
 	</style>
 </head>
 <body>
-	<div class="match-bar">
-		<a href="/dataList/index?page=2
-		
-		">2</a>
-<!-- 		<label for="weibo">
-			微博
-			<input id="weibo" type="checkbox" name="type" value="102">
-		</label>
-		
-		<input type="checkbox" name="type" value="104">
-		<input type="checkbox" name="type" value="108">
-		<input type="checkbox" name="type" value="110"> -->
-	</div>
+	 <?php
+		$arr = [];
+		foreach($detail as $key=> $item){
+			$arr[$key] = $item['id'];
+		}
+		$types = explode(',',$type);
+	 ?>
+	 <div class='selecte'>
+<!-- 		 <div class='main-left'>
+			 
+		 </div> -->
+		 <div class='box1 hide'>
+			 <?php foreach($detail as $key=>$item){?>
+			 <div class="<?php echo in_array($item['id'],$types) ? 'active':'cur'?>">
+				<a href="<?php echo '/dataList/index?page=1&type='.(in_array($item['id'],$types) ? implode(',',array_diff($types,[$item['id']])) :(!empty($type)?$type.','.$item['id']:$item['id']))?>" ><?php echo $item['name']?></a>			 
+			 </div>
+			 <?php }?>		
+	<!-- 		 <div class='whiteBox'>
+				 <?php foreach($detail as $key=>$item){?>
+					<div class='box-item' data-id="<?php echo $item['id']?>"">
+					<?php if(in_array($item['id'],explode(',',$type))){?>
+					 <label  for="<?php echo $item['id']?>" >	
+						<input id="<?php echo $item['id']?>" checked type="checkbox" name="type" value="<?php echo $item['id']?>">
+						<?php echo $item['name']?>
+					 </label>
+					 <?php }else{?>
+					 <label  for="<?php echo $item['id']?>">
+						<input id="<?php echo $item['id']?>" type="checkbox" name="type" value="<?php echo $item['id']?>">
+						<?php echo $item['name']?>
+					 </label>
+					 <?php }?>
+					 </div>
+				 <?php }?>
+			 </div>	 -->		
+		 </div>
+		 <div class='main-right' onclick="changeStatus()">
+			 <span></span>
+		 </div>
+		 <div class='detele' >
+			 <a href="/dataList/index?page=1&type=">
+				 删
+			 </a>
+		 </div>
+	 </div>
 	<div class="main">
-		<?php foreach($result as $item):?>
-		<div class="box">
-			<div class="img-box">
-				<img src="<?php echo $item['post'].'?'.$imgStr?>" alt="<?php echo $item['detail']?>">
-			</div>
-			<div class="detail-box">
-				<div class="name"><?php echo $item['name']?></div>
-				<div class="detail">
-					<div class="detail-title">
-						来源:<?php echo $item['detail']?>
+		<?php if($result==[]){?>
+			<div class='nomore'>暂时没有更多数据啦...</div>
+			<?php }else{?>
+			<?php foreach($result as $item):?>
+				<a href="<?php echo $item['url'];?>" target="_blank">
+					<div class="box">
+						<div class="img-box">
+							<img src="<?php echo $item['post'].'?'.$imgStr?>" alt="<?php echo $item['detail']?>">
+						</div>
+						<div class="detail-box">
+							<div class="name"><?php echo $item['name']?></div>
+							<div class="detail">
+								<div class="detail-title">
+									来源:<?php echo $item['detail']?>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+				</a>
+			<?php endforeach;?>
+		<?php }?>
+	</div>
+	<div class='box'>
+		<?php if($result==[]){?>
+			   <div></div>
+		<?php }else{?>
+			  <ul class='pagination'>
+				  <?php if($page==1){?>
+				  <li  class='page-li page-prev'>上一页</li>
+				  <?php }else{?>
+				   <li  class='page-li page-prev'  onclick='prev()'>上一页</li>
+				   <?php }?>
+				   
+				   <?php if(($count/$limit < 5)){?>			   
+						<?php for($i=0;$i<ceil($count/$limit);$i++){?>
+							<?php if($page==$i){?>
+								<li class='page-li page-number page-active' onclick='numberJump(this)'><?php echo $page;?></li>						
+							<?php }else{?>
+								<li class='page-li page-number'  onclick='numberJump(this)'><?php echo $i+1;?></li>
+							<?php }?>
+						<?php }?>
+					<?php }else{?>
+						<?php if($page>3){?>
+							<li class='page-li number-ellipsis'>...</li>
+							<?php if($page<=ceil($count/$limit)-2){?>
+								<?php for($i=$page-3;$i<$page+2;$i++){?>
+									<?php if($page==$i+1){?>
+										<li class='page-li page-number page-active'  onclick='numberJump(this)'><?php echo $page;?></li>						
+									<?php }else{?>
+										<li class='page-li page-number'  onclick='numberJump(this)'><?php echo $i+1;?></li>
+									<?php }?>
+								<?php }?>
+							<?php }else{?>
+									<?php for($i=ceil($count/$limit)-4;$i<=ceil($count/$limit);$i++){?>
+										<?php if($page==$i){?>
+											<li class='page-li page-number page-active'  onclick='numberJump(this)'><?php echo $page;?></li>						
+										<?php }else{?>
+											<li class='page-li page-number'  onclick='numberJump(this)'><?php echo $i;?></li>
+										<?php }?>
+									<?php }?>
+							<?php }?>
+							<?php if($page<ceil($count/$limit)-2){?>
+								<li class='page-li number-ellipsis'>...</li>
+							<?php }?>
+						<?php }else{?>
+							<?php for($i=0;$i<=5;$i++){?>
+								<?php if($page==$i+1){?>
+									<li class='page-li page-number page-active'  onclick='numberJump(this)'><?php echo $page;?></li>						
+								<?php }else{?>
+									<li class='page-li page-number'  onclick='numberJump(this)'><?php echo $i+1;?></li>
+								<?php }?>
+							<?php }?>
+							<li class='page-li number-ellipsis'>...</li>
+						<?php }?>
+				    <?php }?>
+				  <?php if($page==ceil($count/$limit)){?>
+				  <li class='page-li page-next'>下一页</li>
+				  <?php }else{?>
+				   <li class='page-li page-next'  onclick='next()'>下一页</li>
+				   <?php }?>
+				   <li class='jump'>
+					   <span onclick='jump()'>跳转</span>
+					   <input id='input' value='' type="number" onsubmit="submit" maxlength=3>		
+						<span>页</span>
+				   </li>
+			  </ul>
+		<?php }?>
+	</div>
+	<div class='toUp'>
+		<div class=' to-top'>
+			<div class='up'>UP &uarr;</div>
 		</div>
-		<?php endforeach;?>
 	</div>
 </body>
-<script src="<?php echo base_url('public/pagination.js');?>"></script>
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+<script src="<?php echo base_url('public/simp.js');?>"></script>
+<script src="<?php echo base_url('public/jquery.toTop.js');?>"></script>
+<script>	
+	var _count="<?php echo $count;?>"
+	var _limit="<?php echo $limit;?>"
+	var _type="<?php echo $type;?>"	
+	var _page="<?php echo $page;?>"
+	function changeStatus(){
+		if(!$('.box1').hasClass('hide')){
+			$('.main-right').removeClass('rote')
+			$('.box1').addClass('hide')
+		}else{
+			$('.main-right').addClass('rote')
+			$('.box1').removeClass('hide')
+		}		
+	}
+	function numberJump(e){
+		var value=e.innerHTML
+		window.location.href=window.location.origin+'/dataList/index?page='+value+'&type='+_type
+	}
+	function jump(){		
+		var value=$('#input')[0].value
+		if( value==='0' || value>Math.ceil(_count/_limit) ){return false}else{
+			window.location.href=window.location.origin+'/dataList/index?page='+value+'&type='+_type
+		}
+	}
+	function prev(){
+		console.log('上一页')
+		console.log(window.location.origin)	
+		window.location.href=window.location.origin+'/dataList/index?page='+(getParams('page')*1-1)+'&type='+_type
+	}
+	function next(){
+		console.log('下一页')
+		console.log(window.location.origin)
+		window.location.href=window.location.origin+'/dataList/index?page='+(getParams('page')*1+1)+'&type='+_type
+	}
+	// 地址栏参数
+	function getParams(organizeCode) {
+	    let search = window.location.search.replace(/^\?/, ""); 
+	    // organizeCode=44030022&organizeName=%22%E9%BB%84%E5%9F%94%E7%9C%8B%E5%AE%88%E6%89%80%22
+	
+	    let pairs = search.split("&"); 
+	    // ["organizeCode=44030022", "organizeName=%22%E9%BB%84%E5%9F%94%E7%9C%8B%E5%AE%88%E6%89%80%22"]
+	
+	    let paramsMap = pairs.map(pair => {
+	        let [key, value] = pair.split("=");
+	        return [decodeURIComponent(key), decodeURIComponent(value)];
+	    }).reduce((res, [key, value]) => Object.assign(res, { [key]: value }), {});
+	    // {organizeCode: "44030022", organizeName: ""黄埔看守所""}
+	
+	    return paramsMap[organizeCode] || ""; 
+	    // 44030022
+	}
+	$(function(){
+		if(getParams('page') % 1 !== 0){			
+			window.alert('请输入正确的页码')
+			setTimeout(()=>{
+				window.location.href="http://127.0.0.1/dataList/index?page="+1;
+			},100)
+		}
+		$('.to-top').toTop({
+		    //options with default values
+		    autohide: true,  //boolean 'true' or 'false'
+		    offset: 420,     //numeric value (as pixels) for scrolling length from top to hide automatically
+		    speed: 500,      //numeric value (as mili-seconds) for duration		
+			position:true,   //boolean 'true' or 'false'. Set this 'false' if you want to add custom position with your own css
+			right: 15,       //numeric value (as pixels) for position from right. It will work only if the 'position' is set 'true'
+			bottom: 30       //numeric value (as pixels) for position from bottom. It will work only if the 'position' is set 'true'
+		})
+	})
+</script>
 </html>
