722619ba3873d3e068e53e86df63c4f70b9f27a4
DESKTOP-0BVKIB9\Admin <marbanana.one@gmail.com>
2020-08-24 13:09:40 +0800

    ceratedDate

--- application/views/dataview.php
+++ application/views/dataview.php
@@ -355,7 +355,7 @@ $this->load->helper('url');
 							</div>
 							<div class="detail">								
 								<div class="detail-title">来源:<?php echo $item['detail']?></div>
-								<div class='date'><?php echo date('m-d H:i:s',$item['date']/1000)?></div>								
+								<div class='date'><?php echo $item['createDate']?></div>								
 							</div>
 						</div>
 					</div>
